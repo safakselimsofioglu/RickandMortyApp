22b0e19bab3bc40e891230a9bb61edaa8db8f22f
safakselimsofioglu <safak.sofioglu@icloud.com>
2024-07-03 23:06:02 +0300

    Created Rick and Morty project

--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -12,7 +12,7 @@ final class RMTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTabs()
-        print("--")
+        print("--s")
     }
     
     private func setUpTabs(){
